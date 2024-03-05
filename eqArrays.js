83805d616519f742a2b7d2d406daf7f5e8792fd2
animward <142351228+animward@users.noreply.github.com>
2024-03-05 15:02:17 -0800

    Added assertEqual function

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,12 @@
 // FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 const eqArrays = function(arr1, arr2) {
     // Check if the arrays have the same length
     if (arr1.length !== arr2.length) {
