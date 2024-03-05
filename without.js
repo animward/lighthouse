f2e2a58b840e70c4f0129118098f8d905f16e7d4
animward <142351228+animward@users.noreply.github.com>
2024-03-05 15:25:58 -0800

    Made outputs clearer

--- without.js
+++ without.js
@@ -10,11 +10,12 @@ const without = function(source, itemsToRemove) {
 
   return result;
 };
+
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log("Assertion Passed: Arrays are equal");
+    console.log(`Assertion Passed: ${arr1} and ${arr2} are equal`);
   } else {
-    console.log("Assertion Failed: Arrays are not equal");
+    console.log(`Assertion Failed: ${arr1} and ${arr2} are not equal`);
   }
 };
 
