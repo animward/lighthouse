39ca07fc79073964322e5883c3d8d77220f4ce91
EgoExperiments <egoexperiments@gmail.com>
2024-03-12 13:33:45 -0700

    create callback function using user input

--- vagrant/timer1.js
+++ vagrant/timer1.js
@@ -1,18 +1,36 @@
 // grabbed from altacademy.com
+
+/*const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('What is your name?', name => {
+  console.log(`Hello, ${name}!`);
+  readline.close();
+});*/
+
+
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-console.log("⸻TIMER⸻\n")
+function getTimerDuration(timerHandler) {
+    console.log("⸻TIMER⸻\n")
 
-readline.question('How long do you want your timer to be??\n', time => {
-    if (!isNaN(time) && Number.isInteger(Number(time))) {
-        console.log(`\nOkay, ${time} countdown starting now!`);
-        readline.close();
-    } else {
-        console.log("PLEASE ENTER A VALID NUMBER!");
-        readline.close();
-    }
-  readline.close();
-});
+    readline.question('How long do you want your timer to be??\n', time => {
+        if (!isNaN(time) && Number.isInteger(Number(time))) { // check for valid number
+            timerHandler(parseInt(time, 10));
+            readline.close();
+        } else {
+            console.log("PLEASE ENTER A VALID NUMBER!"); // error if not valid number
+            readline.close();
+        }
+    });
+}
+function startTimer(duration) {
+    console.log(`\nOkay, ${duration} second countdown starting now!`);
+}
+
+getTimerDuration(startTimer);
