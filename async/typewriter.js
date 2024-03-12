7c678e544b2cb29f2cff1aabadc187484e452afa
EgoExperiments <egoexperiments@gmail.com>
2024-03-12 12:10:46 -0700

    added the for of loop

--- async/typewriter.js
+++ async/typewriter.js
@@ -1,5 +1,7 @@
 const sentence = "hello there from lighthouse labs";
 
 for (const char of sentence) {
-  process.stdout.write(char);
+    setTimeout(() => {
+    process.stdout.write(char); // print the char here
+}, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
 }
