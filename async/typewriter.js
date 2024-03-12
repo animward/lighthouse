a43b107d39e06cca3fd69183000af054cffdfdcd
EgoExperiments <egoexperiments@gmail.com>
2024-03-12 12:16:02 -0700

    c style loop

--- async/typewriter.js
+++ async/typewriter.js
@@ -1,7 +1,12 @@
 const sentence = "hello there from lighthouse labs";
 
-for (const char of sentence) {
+
+// change to c style
+for (let i = 0; i < sentence.length; i++) {
     setTimeout(() => {
-    process.stdout.write(char); // print the char here
-}, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
+        process.stdout.write(sentence[i]); // print the char here
+        if (i === sentence.length - 1) {
+            process.stdout.write('\n');
+        }
+    }, 50 * i) // <= 1s delay to make it noticeable. Can dial it down later.
 }
