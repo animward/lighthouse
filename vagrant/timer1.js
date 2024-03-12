cb3de2b536a895d034029a36eca5e8e2bde9da04
EgoExperiments <egoexperiments@gmail.com>
2024-03-12 13:20:00 -0700

    check isNaN, if not, console log an error

--- vagrant/timer1.js
+++ vagrant/timer1.js
@@ -7,6 +7,12 @@ const readline = require('readline').createInterface({
 console.log("⸻TIMER⸻\n")
 
 readline.question('How long do you want your timer to be??\n', time => {
-  console.log(`\nOkay, ${time} countdown starting now!`);
+    if (!isNaN(time) && Number.isInteger(Number(time))) {
+        console.log(`\nOkay, ${time} countdown starting now!`);
+        readline.close();
+    } else {
+        console.log("PLEASE ENTER A VALID NUMBER!");
+        readline.close();
+    }
   readline.close();
 });
