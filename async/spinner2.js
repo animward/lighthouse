db4cf361231660400210327e405a4f172cecccbe
EgoExperiments <egoexperiments@gmail.com>
2024-03-12 12:35:59 -0700

    delete everything add function, this no longer will work

--- async/spinner2.js
+++ async/spinner2.js
@@ -1,27 +1,8 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1100);
-
 // implement array
 const spinnerFrames = ['|', '/', '-', '\\', '|', '-'];
+
+function animate(index) {
+    setTimeout(() => {
+    process.stdout.write('\r|   ');
+    }, 100);
+}
