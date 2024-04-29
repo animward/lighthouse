f291722b2d7274895fdc488701566c4464a1cbe5
animward <animward@gmail.com>
2024-04-29 16:33:49 -0700

    refactor method and replace with attr_writer

--- ruby/instance_variables.rb
+++ ruby/instance_variables.rb
@@ -1,14 +1,10 @@
 class Person
   attr_reader :name
+  attr_writer :name
 
   def initialize(name)
     @name = name
   end
-
-  def name=(new_name)
-    @name = new_name
-  end
-end
 
 p = Person.new('L. Ron')
 puts p.name
